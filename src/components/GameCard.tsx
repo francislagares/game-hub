8b876d75ed49753bec72c1636897c0ddfae6c13c
Francis Lagares <francis.lagares@gmail.com>
2023-05-28 21:23:01 +0200

    feat: create image service to optimize images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, HStack, Heading, Image } from '@chakra-ui/react';
 
 import { Game } from '@/models/Game';
+import getCroppedImageUrl from '@/services/image.service';
 
 import CriticScore from './CriticScore';
 import PlatformIconList from './PlatformIconList';
@@ -12,7 +13,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow='hidden'>
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize='2xl'>{game.name}</Heading>
         <HStack justifyContent='space-between'>

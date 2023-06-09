a2c09636d46f0a914662186d6570232a6885a9cf
Francis Lagares <francis.lagares@gmail.com>
2023-06-09 21:24:09 +0200

    feat: @tanstack/react-query setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import theme from './theme';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initalColorMode} />
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ColorModeScript initialColorMode={theme.config.initalColorMode} />
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 );

2dc4ede88667508129adde446fe82bfad6f9c42b
Francis Lagares <francis.lagares@gmail.com>
2023-06-29 17:46:59 +0200

    fix: search input bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 
 import useGameQueryStore from '@/store';
 import { SubmitEvt } from '@/types/events';
@@ -9,12 +10,14 @@ import { SubmitEvt } from '@/types/events';
 const SearchInput = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore(s => s.setSearchText);
+  const navigate = useNavigate();
 
   const handleFormSubmit = (event: SubmitEvt) => {
     event.preventDefault();
 
     if (inputRef.current) {
       setSearchText(inputRef.current.value);
+      navigate('/');
     }
   };
 
@@ -27,7 +30,6 @@ const SearchInput = () => {
           borderRadius={20}
           placeholder='Search games...'
           variant='filled'
-          value={inputRef.current?.value}
         />
       </InputGroup>
     </form>
